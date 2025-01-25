778cf2aebe45c58b726c2ef420a78b6314f3fc1c
mrlondon120 <orinlondon@gmail.com>
2025-01-25 16:14:54 -0500

    Update flux.sh

--- config/provisioning/flux.sh
+++ config/provisioning/flux.sh
@@ -19,8 +19,39 @@ PIP_PACKAGES=(
 )
 
 NODES=(
-    
-)
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+	"https://github.com/cubiq/ComfyUI_essentials"
+	"https://github.com/Gourieff/comfyui-reactor-node"
+	"https://github.com/ltdrdata/ComfyUI-Impact-Pack"
+	"https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes"
+	"https://github.com/rgthree/rgthree-comfy"
+	"https://github.com/crystian/ComfyUI-Crystools"
+	"https://github.com/giriss/comfy-image-saver"
+	"https://github.com/Fannovel16/comfyui_controlnet_aux"
+	"https://github.com/WASasquatch/was-node-suite-comfyui"
+	"https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+	"https://github.com/melMass/comfy_mtb"
+	"https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
+	"https://github.com/sipherxyz/comfyui-art-venture"
+	"https://github.com/twri/sdxl_prompt_styler"
+	"https://github.com/hylarucoder/comfyui-copilot"
+	"https://github.com/kijai/ComfyUI-KJNodes"
+	"https://github.com/KoreTeknology/ComfyUI-Universal-Styler"
+    "https://github.com/PowerHouseMan/ComfyUI-AdvancedLivePortrait"
+	"https://github.com/yolain/ComfyUI-Easy-Use"
+    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
+	"https://github.com/chrisgoringe/cg-use-everywhere"
+	"https://github.com/huanngzh/ComfyUI-MVAdapter"
+    "https://github.com/Limitex/ComfyUI-Diffusers"
+	"https://github.com/lldacing/ComfyUI_BiRefNet_ll"
+    "https://github.com/ltdrdata/ComfyUI-Impact-Subpack"
+	"https://github.com/spacepxl/ComfyUI-Image-Filters"
+    "https://github.com/liusida/ComfyUI-AutoCropFaces"
+	"https://github.com/cubiq/ComfyUI_IPAdapter_plus"
+	"https://github.com/cubiq/ComfyUI_FaceAnalysis"
+	"https://github.com/un-seen/comfyui-tensorops"
+     "https://github.com/civitai/civitai_comfy_nodes"
+	"https://github.com/balazik/ComfyUI-PuLID-Flux")
 
 CHECKPOINT_MODELS=(
 )
